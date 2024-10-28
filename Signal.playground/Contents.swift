429b561696c14b2813f393d3c0785eafc77b96ed
SENA <sena.web0310@gmail.com>
2024-10-29 00:39:18 +0900

    Signal.playground更新

--- Signal.playground/Contents.swift
+++ Signal.playground/Contents.swift
@@ -1,45 +1,74 @@
 import UIKit
 
-class Signal {
-    enum SignalType {
-        case blue
-        case yellow
-        case red
-    }
-    
-    var inputSignal: String = ""
-    
-    var inputBlue = "青"
-    var inputYellow = "黄"
-    var inputRed = "赤"
-    
-    var walkBlue = "青"
-    var walkRed = "赤"
-    
-    func signalSwitch(inputSignal: String) -> Bool {
-        self.inputSignal = inputSignal
-        var isSignal = false
-        if inputSignal == inputBlue {
-            isSignal = inputBlue == inputSignal
-            print("車道の信号は\(signal.inputSignal)")
-            print("歩行者信号は\(walkRed)")
-        } else if inputSignal == inputYellow {
-            isSignal = inputYellow == inputSignal
-            print("車道の信号は\(signal.inputSignal)")
-            print("歩行者信号は\(walkRed)")
-        } else if inputSignal == inputRed {
-            isSignal = inputRed == inputSignal
-            print("車道の信号は\(signal.inputSignal)")
-            print("歩行者信号は\(walkBlue)")
-        } else {
-            print("信号機故障中")
+
+// Global scope
+enum SignalType {
+    case blue
+    case yellow
+    case red
+
+    var price: Int {
+        switch self {
+        case .blue: return 1
+        case .yellow: return 2
+        case .red: return 3
         }
-        
-        return isSignal
     }
 }
 
-let signal = Signal()
-let isSuccessSignal = signal.signalSwitch(inputSignal: "青")
+// Global scope
+struct SignalModel {
+    var type: SignalType
+}
+
+// Global scope
+protocol StopSignalValidatable {}
+extension StopSignalValidatable {
+    func validateSignal(with drink: SignalModel,
+                              inputSignal: Int) -> Bool {
+        guard drink.type.price == inputSignal else {
+            print("信号機故障中")
+            return false
+        }
+        return drink.type.price < inputSignal
+    }
+}
+
+class VirtualVendingMachine: StopSignalValidatable {
+    private var inputedSignal: Int = 0
+    private var blue = SignalModel(type: .blue)
+    private var yellow = SignalModel(type: .yellow)
+    private var red = SignalModel(type: .red)
+    
+    private var otherYen: Int = 0
+
+    func buyDrink(type: SignalType, inputedSignal: Int) -> Bool {
+        switch type {
+        case .blue:
+            let isSignal = validateSignal(with: blue, inputSignal: inputedSignal)
+            if inputedSignal == 1 {
+                print("青信号")
+            }
+            return isSignal
+        case .yellow:
+            let isSignal = validateSignal(with: yellow, inputSignal: inputedSignal)
+            if inputedSignal == 2 {
+                print("黄信号")
+            }
+            return isSignal
+        case .red:
+            let isSignal = validateSignal(with: red, inputSignal: inputedSignal)
+            if inputedSignal == 3 {
+                print("赤信号")
+            }
+            return isSignal
+        }
+    }
+
+}
+
+let virtualVendingMachine = VirtualVendingMachine()
+let isSuccessToBuy = virtualVendingMachine.buyDrink(type: .blue, inputedSignal: 1)
+
 
 
